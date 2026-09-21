2bfed77af309b6fc5a35c11287685c07d9e6ab60
NoobGangster12 <memefs13438@gmail.com>
2026-09-20 22:11:55 -0300

    Criação de listagem

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -1,6 +1,8 @@
 
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane;
+
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -219,6 +221,7 @@ public class listagemVIEW extends javax.swing.JFrame {
                 });
             }
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao carregar tabela: " + e.getMessage());
         }
     
     }
